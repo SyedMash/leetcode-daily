7a1d57d6e503bcfab135919c9a33ac14a2efc7e0
SyedMash <mashrtx7@gmail.com>
2025-08-24 15:30:29 +0500

    rob-house

--- house_robber/main.py
+++ house_robber/main.py
@@ -18,10 +18,19 @@
 
 
 def rob(nums: list[int]) -> int:
-    max_robbed_amount = 0
-    
+    if not nums:
+        return 0
+    if len(nums) == 1:
+        return nums[0]
 
-    return max_robbed_amount
+    max_money = [0] * len(nums)
+    max_money[0] = nums[0]
+    max_money[1] = max(nums[0], nums[1])
+
+    for i in range(2, len(nums)):
+        max_money[i] = max(max_money[i - 1], nums[i] + max_money[i - 2])
+
+    return max_money[-1]
 
 
-print(rob(nums=[1, 2, 3, 1]))
+print(rob(nums=[1, 1, 1]))
